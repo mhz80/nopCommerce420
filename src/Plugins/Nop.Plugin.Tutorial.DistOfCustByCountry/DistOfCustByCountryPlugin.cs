e83af068e282cab77d0aed792a08fa4e486cdebc
mostafa.hoseinzade <mostafa.hoseinzade@NO242-PC>
2020-07-26 16:37:18 +0430

    InitialPluginSetup

---
+++ src/Plugins/Nop.Plugin.Tutorial.DistOfCustByCountry/DistOfCustByCountryPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Tutorial.DistOfCustByCountry
+{
+    public class DistOfCustByCountryPlugin : BasePlugin
+    {
+        public DistOfCustByCountryPlugin()
+        {
+
+        }
+
+        public override string GetConfigurationPageUrl()
+        {
+            return $"{_webHelper.GetStoreLocation()}Admin/DistOfCustBuCountryPlugin/Configure";
+        }
+
+        public override void Install()
+        {
+            //Code you want to run while installing the plugin goes here.
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            //Code you want to run while Uninstalling the plugin goes here.
+            base.Uninstall();
+        }
+    }
+}
