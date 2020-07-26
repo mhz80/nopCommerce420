8c1471a205e815ee4a83ec582423032db30df0c6
mostafa.hoseinzade <mostafa.hoseinzade@NO242-PC>
2020-07-26 14:15:26 +0430

    MakeItWidget

---
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/Components/HelloWorldWidgetViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widget.HelloWorld.Components
+{
+    [ViewComponent(Name = "HelloWorldWidget")]
+    public class HelloWorldWidgetViewComponent : NopViewComponent
+    {
+        public IViewComponentResult Invoke(string widgetZone)
+        {
+            return Content("Hello World");
+        }
+
+    }
+}
--- src/Plugins/Nop.Plugin.Widget.HelloWorld/HelloWorldPlugin.cs
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/HelloWorldPlugin.cs
@@ -1,9 +1,35 @@
-﻿using Nop.Services.Plugins;
+﻿using System.Collections.Generic;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
 
 namespace Nop.Plugin.Widget.HelloWorld
 {
-    public class HelloWorldPlugin : BasePlugin
+    public class HelloWorldPlugin : BasePlugin, IWidgetPlugin
     {
+        /// <summary>
+        /// Gets a value indicating whether to hide this plugin on the widget list page in the admin area
+        /// </summary>
+        public bool HideInWidgetList => false;
+
+        /// <summary>
+        /// Gets widget zones where this widget should be rendered
+        /// </summary>
+        /// <returns>Widget zones</returns>
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            return "HelloWorldWidget";
+        }
+
+        /// <summary>
+        /// Gets a name of a view component for displaying widget
+        /// </summary>
+        /// <param name="widgetZone">Name of the widget zone</param>
+        /// <returns>View component name</returns>
+        public IList<string> GetWidgetZones()
+        {
+            return new List<string> { "home_page_before_categories" };
+        }
+
         public override void Install()
         {
             //Logic during installation goes here...
@@ -18,5 +44,6 @@ namespace Nop.Plugin.Widget.HelloWorld
             base.Uninstall();
         }
     }
+
 
 }
