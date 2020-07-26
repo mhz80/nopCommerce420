bfd53abcbfbbf713ae889d7fd154ff7f9dea2f88
mostafa.hoseinzade <mostafa.hoseinzade@NO242-PC>
2020-07-26 16:39:20 +0430

    AddModels

---
+++ src/Plugins/Nop.Plugin.Tutorial.DistOfCustByCountry/Models/CustomersByCountrySearchModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Web.Framework.Models;
+
+namespace Nop.Plugin.Tutorial.DistOfCustByCountry.Models
+{
+    public class CustomersByCountrySearchModel : BaseSearchModel
+    {
+    }
+
+}
---
+++ src/Plugins/Nop.Plugin.Tutorial.DistOfCustByCountry/Models/CustomersDistribution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Web.Framework.Models;
+
+namespace Nop.Plugin.Tutorial.DistOfCustByCountry.Models
+{
+    public class CustomersDistribution : BaseNopModel
+    {
+        /// <summary>
+        /// Country based on the billing address.
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Number of customers from specific country.
+        /// </summary>
+        public int NoOfCustomers { get; set; }
+    }
+
+}
