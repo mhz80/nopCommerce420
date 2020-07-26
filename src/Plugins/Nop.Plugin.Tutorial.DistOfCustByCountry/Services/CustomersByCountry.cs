bcfe8d4f420b0c3971c1dcab68f0b84f5d4ee00e
mostafa.hoseinzade <mostafa.hoseinzade@NO242-PC>
2020-07-26 16:53:00 +0430

    AddService

---
+++ src/Plugins/Nop.Plugin.Tutorial.DistOfCustByCountry/Services/CustomersByCountry.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nop.Plugin.Tutorial.DistOfCustByCountry.Models;
+using Nop.Services.Common;
+using Nop.Services.Customers;
+using Nop.Services.Directory;
+
+namespace Nop.Plugin.Tutorial.DistOfCustByCountry.Services
+{
+    public class CustomersByCountry : ICustomersByCountry
+    {
+        private readonly IAddressService _addressService;
+        private readonly ICountryService _countryService;
+        private readonly ICustomerService _customerService;
+
+        public CustomersByCountry(IAddressService addressService,
+            ICountryService countryService,
+            ICustomerService customerService)
+        {
+            _addressService = addressService;
+            _countryService = countryService;
+            _customerService = customerService;
+        }
+
+        public List<CustomersDistribution> GetCustomersDistributionByCountry()
+        {
+            return _customerService.GetAllCustomers()
+                .Where(c => c.ShippingAddressId != null)
+                .Select(c => new
+                {
+                    _countryService.GetCountryByAddress(_addressService.GetAddressById(c.ShippingAddressId ?? 0))
+                        .Name,
+                    c.Username
+                })
+                .GroupBy(c => c.Username)
+                .Select(cbc => new CustomersDistribution { Country = cbc.Key, NoOfCustomers = cbc.Count() }).ToList();
+        }
+    }
+
+}
---
+++ src/Plugins/Nop.Plugin.Tutorial.DistOfCustByCountry/Services/ICustomersByCountry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Plugin.Tutorial.DistOfCustByCountry.Models;
+
+namespace Nop.Plugin.Tutorial.DistOfCustByCountry.Services
+{
+    public interface ICustomersByCountry
+    {
+        List<CustomersDistribution> GetCustomersDistributionByCountry();
+    }
+}
