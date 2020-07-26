260a39973b3e958842b53be14765e47e6860780a
mostafa.hoseinzade <mostafa.hoseinzade@NO242-PC>
2020-07-26 13:48:16 +0430

    AddClassFromBasePlugin

---
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/HelloWorldPlugin.cs
@@ -0,0 +1,22 @@
+﻿using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Widget.HelloWorld
+{
+    public class HelloWorldPlugin : BasePlugin
+    {
+        public override void Install()
+        {
+            //Logic during installation goes here...
+
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            //Logic during uninstallation goes here...
+
+            base.Uninstall();
+        }
+    }
+
+}
