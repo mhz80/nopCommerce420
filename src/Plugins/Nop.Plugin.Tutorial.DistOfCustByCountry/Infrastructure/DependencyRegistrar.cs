cb24fa20dd7dd176a765b3e0ae7a8c2d710405aa
mostafa.hoseinzade <mostafa.hoseinzade@NO242-PC>
2020-07-27 10:59:56 +0430

    DependencyRegisteration

---
+++ src/Plugins/Nop.Plugin.Tutorial.DistOfCustByCountry/Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+using Nop.Core.Infrastructure.DependencyManagement;
+using Nop.Plugin.Tutorial.DistOfCustByCountry.Services;
+
+namespace Nop.Plugin.Tutorial.DistOfCustByCountry.Infrastructure
+{
+    class DependencyRegistrar : IDependencyRegistrar
+    {
+        public int Order => 1;
+
+        public void Register(ContainerBuilder builder, ITypeFinder typeFinder, NopConfig config)
+        {
+            builder.RegisterType<CustomersByCountry>().As<ICustomersByCountry>().InstancePerLifetimeScope();
+        }
+    }
+
+}
