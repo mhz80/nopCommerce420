746168fa06b39c8a1d923fd728a045ccc245c9b0
mostafa.hoseinzade <mostafa.hoseinzade@NO242-PC>
2020-07-27 11:08:08 +0430

    AddRoute

---
+++ src/Plugins/Nop.Plugin.Tutorial.DistOfCustByCountry/Infrastructure/RouteProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Mvc.Routing;
+
+
+namespace Nop.Plugin.Tutorial.DistOfCustByCountry.Infrastructure
+{
+    public class RouteProvider : IRouteProvider
+    {
+        /// <summary>
+        /// Register routes
+        /// </summary>
+        /// <param name="endpointRouteBuilder">Route builder</param>
+        public void RegisterRoutes(IRouteBuilder routeBuilder)
+        {
+            //add route for the access token callback
+            //add route for the access token callback
+            routeBuilder.MapRoute("CustomersDistributionByCountry", "Plugins/Tutorial/CustomerDistByCountry/",
+                new { controller = "TutorialCustomersByCountry", action = "GetCustomersCountByCountry" });
+        }
+
+        /// <summary>
+        /// Gets a priority of route provider
+        /// </summary>
+        public int Priority => 0;
+    }
+
+}
